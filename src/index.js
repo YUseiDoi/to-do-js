50492936fdc34a81155a869bf3657f88e24db023
YUseiDoi <72484394+YUseiDoi@users.noreply.github.com>
2020-11-26 01:06:31 +0900

    udemyセクション5.32まで

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの値を取得
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // div作成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // liタグ作成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  // ボタンタグの作成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert();
+  });
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert();
+  });
+
+  // divタグの下にliタグ
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+  console.log(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
